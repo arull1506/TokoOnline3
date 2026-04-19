5efc59d3b098c4cc03705cdbf89053fb980cb798
Wail Kasfury <wkazfury@gmail.com>
2026-04-19 23:07:56 +0700

    first commit

--- resources/views/v_layouts/app.blade.php
+++ resources/views/v_layouts/app.blade.php
@@ -81,24 +81,37 @@
                         <!-- /Cart -->
 
                         <!-- Account -->
-                        <li class="header-account dropdown default-dropdown">
-                            <div class="dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="true">
-                                <div class="header-btns-icon">
-                                    <i class="fa fa-user-o"></i>
-                                </div>
-                                <strong class="text-uppercase">Akun Saya<i class="fa fa-caret-down"></i></strong>
-                            </div>
-                            <a href="#" class="text-uppercase">Login</a>
-                            <ul class="custom-menu">
-                                <li><a href="#"><i class="fa fa-user-o"></i> My Account</a></li>
-                                <li><a href="#"><i class="fa fa-heart-o"></i> My Wishlist</a></li>
-                                <li><a href="#"><i class="fa fa-exchange"></i> Compare</a></li>
-                                <li><a href="#"><i class="fa fa-check"></i> Checkout</a></li>
-                                <a href="{{ route('auth.redirect') }}" class="text-uppercase">Login</a>
-                                <li><a href="#"><i class="fa fa-user-plus"></i> Create An Account</a></li>
-                            </ul>
-                        </li>
-                        <!-- /Account -->
+@if (Auth::check())
+<li class="header-account dropdown default-dropdown">
+    <div class="dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="true">
+        <div class="header-btns-icon">
+            <i class="fa fa-user-o"></i>
+        </div>
+        <strong class="text-uppercase">{{ Auth::user()->nama }}<i class="fa fa-caret-down"></i></strong>
+    </div>
+    <ul class="custom-menu">
+        <li>
+            <a href="#" onclick="event.preventDefault(); document.getElementById('keluar-app').submit();">
+                <i class="fa fa-power-off"></i> Keluar
+            </a>
+            <form id="keluar-app" action="{{ route('logout') }}" method="POST" style="display:none;">
+                @csrf
+            </form>
+        </li>
+    </ul>
+</li>
+@else
+<li class="header-account dropdown default-dropdown">
+    <div class="dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="true">
+        <div class="header-btns-icon">
+            <i class="fa fa-user-o"></i>
+        </div>
+        <strong class="text-uppercase">Akun Saya<i class="fa fa-caret-down"></i></strong>
+    </div>
+    <a href="{{ route('auth.redirect') }}" class="text-uppercase">Login</a>
+</li>
+@endif
+<!-- /Account -->
 
                         <!-- Mobile nav toggle-->
                         <li class="nav-toggle">
